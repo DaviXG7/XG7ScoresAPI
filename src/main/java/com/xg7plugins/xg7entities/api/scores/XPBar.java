5c0f789a834f930dbaefbee7fc22dbdfe264e4bb
DaviXG7 <davisonic2010@gmail.com>
2024-08-28 21:52:46 -0300

    ScoreAPI

--- src/main/java/com/xg7plugins/xg7entities/api/scores/XPBar.java
+++ src/main/java/com/xg7plugins/xg7entities/api/scores/XPBar.java
@@ -3,6 +3,7 @@ package com.xg7plugins.xg7entities.api.scores;
 import com.xg7plugins.xg7entities.api.Score;
 import com.xg7plugins.xg7entities.api.ScoreManager;
 import com.xg7plugins.xg7entities.api.utils.ScoreCondition;
+import com.xg7plugins.xg7entities.api.utils.Text;
 import org.bukkit.entity.Player;
 
 public class XPBar extends Score {
@@ -15,8 +16,8 @@ public class XPBar extends Score {
     @Override
     public void update() {
         for (Player player : super.getPlayers()) {
-            player.setLevel(Integer.parseInt(getToUpdate()[getIndexUpdating()].split(", ")[0]));
-            player.setExp(Float.parseFloat(getToUpdate()[getIndexUpdating()].split(", ")[1]));
+            player.setLevel(Integer.parseInt(Text.format(getToUpdate()[getIndexUpdating()].split(", ")[0]).setPlaceholders(player).getText()));
+            player.setExp(Float.parseFloat(Text.format(getToUpdate()[getIndexUpdating()].split(", ")[1]).setPlaceholders(player).getText()));
         }
     }
 }
